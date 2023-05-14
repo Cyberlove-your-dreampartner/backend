46f427dc78f4c9df1d7a56b41c814f253d008c25
kkzzo1o1 <kkzzo1o1o1@gmail.com>
2023-05-14 12:49:30 +0800

    Fix: delete check if system is not existing

--- models/partner.js
+++ models/partner.js
@@ -53,11 +53,10 @@ PartnerSchema.pre("save", async function (next) {
 PartnerSchema.pre("save", async function (next) {
   try {
     const chat = await Chat.findOne({ userId: this.userId });
-    // if system is not existing
-    if(!chat.system){
-      chat.system = `你是USER的女朋友。你稱呼USER為${this.nickname}。你的名子叫${this.name}。你的MBTI是${this.MBTI}。你的職業是${this.job}。你的個性是${this.personality}。禁止提到AI機器人。你的回答要口語化並在兩句內完成。`;
-      await chat.save();
-    }
+
+    chat.system = `你是USER的女朋友。你稱呼USER為${this.nickname}。你的名子叫${this.name}。你的MBTI是${this.MBTI}。你的職業是${this.job}。你的個性是${this.personality}。禁止提到AI機器人。你的回答要口語化並在兩句內完成。`;
+    await chat.save();
+
     next();
   } catch (err) {
     console.log(err);
