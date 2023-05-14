4a77933ba474db92d377e34ca9be986500f7f573
kkzzo1o1 <kkzzo1o1o1@gmail.com>
2023-05-14 12:57:50 +0800

    Fix: merge pre function in parter model

--- models/partner.js
+++ models/partner.js
@@ -44,25 +44,18 @@ PartnerSchema.pre("save", async function (next) {
       image.imgURL = await uploadImg(image.imgBase64);
       await image.save();
     }
-    next();
-  } catch (err) {
-    console.log(err);
-  }
-});
 
-PartnerSchema.pre("save", async function (next) {
-  try {
+    // update chat system
     const chat = await Chat.findOne({ userId: this.userId });
-
     chat.system = `你是USER的女朋友。你稱呼USER為${this.nickname}。你的名子叫${this.name}。你的MBTI是${this.MBTI}。你的職業是${this.job}。你的個性是${this.personality}。禁止提到AI機器人。你的回答要口語化並在兩句內完成。`;
     await chat.save();
 
     next();
   } catch (err) {
     console.log(err);
-    next(err);
   }
 });
+
 
 
 const Partner = mongoose.model("Partner", PartnerSchema);
