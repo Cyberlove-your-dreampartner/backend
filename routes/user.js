9d234f507825c6dd6e8c149999d02f62a8f3e6ba
grace0950 <pinyan0950@gmail.com>
2023-05-17 16:26:56 +0800

    Fix: fix error in user route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const auth = require("../middlewares/auth");
 const userController = require("../controllers/user");
 
 router.post("/register", userController.register);
