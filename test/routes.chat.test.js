92b90ee59874520e19cbc8cd60ee148f55e2ea44
kkzzo1o1 <kkzzo1o1o1@gmail.com>
2023-05-21 16:21:56 +0800

    Fix: update chat api test

--- test/routes.chat.test.js
+++ test/routes.chat.test.js
@@ -18,24 +18,24 @@ const DID = require("../utils/d-id");
 
 describe("GET /chat/imageURL", () => {
   let verifyStub;
-  let findOneStub;
+  let findPartnerStub;
 
   beforeEach(() => {
     // Stub the JWT verification function
     verifyStub = sinon.stub(jwt, "verify");
-    findOneStub = sinon.stub(Partner, "findOne");
+    findPartnerStub = sinon.stub(Partner, "findOne");
   });
 
   afterEach(() => {
     verifyStub.restore();
-    findOneStub.restore();
+    findPartnerStub.restore();
   });
 
   it("should return the image URL if the partner exists", async () => {
     verifyStub.returns({ userId: "fakeUserId" });
 
     // Simulate partner exists
-    findOneStub.resolves({imageId: "fakeImageId" });
+    findPartnerStub.resolves({imageId: "fakeImageId" });
     // Simulate image exists
     sinon.stub(Image, "findOne").resolves({ imgURL: "fakeImgURL" });
 
@@ -50,14 +50,14 @@ describe("GET /chat/imageURL", () => {
     expect(response.body.imgURL).to.equal("fakeImgURL");
     // Verify the function calls and stub invocations
     sinon.assert.calledOnce(verifyStub);
-    sinon.assert.calledOnce(findOneStub);
+    sinon.assert.calledOnce(findPartnerStub);
   });
 
   it("should return an error if the partner does not exist", async () => {
     verifyStub.returns({ userId: "fakeUserId" });
 
     // Simulate partner does not exist
-    findOneStub.resolves(null);
+    findPartnerStub.resolves(null);
 
     // Send a GET request to get imageURL
     const response = await request(app)
@@ -70,14 +70,14 @@ describe("GET /chat/imageURL", () => {
     expect(response.body.message).to.equal("Partner not found");
     // Verify the function calls and stub invocations
     sinon.assert.calledOnce(verifyStub);
-    sinon.assert.calledOnce(findOneStub);
+    sinon.assert.calledOnce(findPartnerStub);
   });
 
   it("should return an internal server error if an exception is thrown", async () => {
     verifyStub.returns({ userId: "fakeUserId" });
 
     // Simulate database error if an exception is thrown
-    findOneStub.throws({
+    findPartnerStub.throws({
       name: "CustomError",
       message: "Custom error message",
     });
@@ -93,7 +93,7 @@ describe("GET /chat/imageURL", () => {
     expect(response.body.message).to.equal("CustomError Custom error message");
     // Verify the function calls and stub invocations
     sinon.assert.calledOnce(verifyStub);
-    sinon.assert.calledOnce(findOneStub);
+    sinon.assert.calledOnce(findPartnerStub);
   });
 });
 
@@ -101,16 +101,16 @@ describe("GET /chat/imageURL", () => {
 
 describe("POST /chat/replyMessage", () => {
   let verifyStub;
-  let findOneStub;
-  let saveStub;
+  let findChatStub;
+  let saveChatStub;
   let insertMessageStub;
   let getReplyStub;
 
   beforeEach(() => {
     // Stub the JWT verification function
     verifyStub = sinon.stub(jwt, "verify");
-    findOneStub = sinon.stub(Chat, "findOne");
-    saveStub = sinon.stub(Chat.prototype, "save");
+    findChatStub = sinon.stub(Chat, "findOne");
+    saveChatStub = sinon.stub(Chat.prototype, "save");
     insertMessageStub = sinon.stub(Chat.prototype, "insertMessage");
     // Stub the getReplyStub function
     getReplyStub = sinon.stub(OPENAI, "getReply");
@@ -118,8 +118,8 @@ describe("POST /chat/replyMessage", () => {
 
   afterEach(() => {
     verifyStub.restore();
-    findOneStub.restore();
-    saveStub.restore();
+    findChatStub.restore();
+    saveChatStub.restore();
     insertMessageStub.restore();
     getReplyStub.restore();
   });
@@ -127,20 +127,20 @@ describe("POST /chat/replyMessage", () => {
   it("should reply to a user message and return the script and config", async () => {
     verifyStub.returns({ userId: "fakeUserId" });
     // Simulate chat does not exist
-    findOneStub.resolves(null);
+    findChatStub.resolves(null);
     // Simulate successful save
-    saveStub.resolves();
+    saveChatStub.resolves();
     // Simulate successful insert message
     insertMessageStub.resolves();
     // Simulate successful get reply
-    getReplyStub.resolves({ role: 'system', content: 'Reply message' });
+    getReplyStub.resolves({ role: "system", content: "Reply message" });
 
 
     // Send a POST request to reply Message
     const response = await request(app)
       .post("/chat/replyMessage")
       .set("authorization", "jwtToken")
-      .send({ message: 'fakeMessage' });
+      .send({ message: "fakeMessage" });
 
     // Perform assertions on the response
     expect(response.status).to.equal(200);
@@ -160,8 +160,8 @@ describe("POST /chat/replyMessage", () => {
     });
     // Verify the function calls and stub invocations
     sinon.assert.calledOnce(verifyStub);
-    sinon.assert.calledOnce(findOneStub);
-    sinon.assert.calledOnce(saveStub);
+    sinon.assert.calledOnce(findChatStub);
+    sinon.assert.calledOnce(saveChatStub);
     sinon.assert.calledTwice(insertMessageStub);
     sinon.assert.calledOnce(getReplyStub);
   });
@@ -170,7 +170,7 @@ describe("POST /chat/replyMessage", () => {
     verifyStub.returns({ userId: "fakeUserId" });
 
     // Simulate database error if an exception is thrown
-    findOneStub.throws({
+    findChatStub.throws({
       name: "CustomError",
       message: "Custom error message",
     });
@@ -179,15 +179,15 @@ describe("POST /chat/replyMessage", () => {
     const response = await request(app)
       .post("/chat/replyMessage")
       .set("authorization", "jwtToken")
-      .send({ message: 'fakeMessage' });
+      .send({ message: "fakeMessage" });
 
     // Perform assertions on the response
     expect(response.status).to.equal(409);
     expect(response.body.message).to.equal("CustomError Custom error message");
     // Verify the function calls and stub invocations
     sinon.assert.calledOnce(verifyStub);
-    sinon.assert.calledOnce(findOneStub);
-    sinon.assert.notCalled(saveStub);
+    sinon.assert.calledOnce(findChatStub);
+    sinon.assert.notCalled(saveChatStub);
     sinon.assert.notCalled(insertMessageStub);
     sinon.assert.notCalled(getReplyStub);
   });
@@ -195,44 +195,187 @@ describe("POST /chat/replyMessage", () => {
 
 
 
-// describe("GET /chat/idlevideo", () => {
+describe("GET /chat/idlevideo", () => {
+  let verifyStub;
+  let findPartnerStub;
+  let findImageStub;
+  let getIdleVideoURLStub;
 
-//   it("should return 404 if partner is not found", async () => {
+  beforeEach(() => {
+    // Remove the existing stubs or wraps before creating new ones
+    sinon.restore();
+    // Stub the JWT verification function
+    verifyStub = sinon.stub(jwt, "verify");
+    findPartnerStub = sinon.stub(Partner, "findOne");
+    findImageStub = sinon.stub(Image, "findOne");
+    getIdleVideoURLStub = sinon.stub(DID, "getIdleVideoURL");
+  });
 
-//     sinon.stub(Partner, "findOne").resolves(null);
-//     const res = await request(app)
-//       .get("/chat/idleVideo")
-//       .set("authorization", jwtTokenForTest);
+  afterEach(() => {
+    verifyStub.restore();
+    findPartnerStub.restore();
+    findImageStub.restore();
+    getIdleVideoURLStub.restore();
+  });
 
-//     expect(res.status).to.equal(404);
-//     expect(res.body.message).to.equal("Partner not found");
+  it("should return an error if the partner is not found", async () => {
+    verifyStub.returns({ userId: "fakeUserId" });
+    findPartnerStub.resolves(null);
 
-//     sinon.restore();
-//   });
+    const response = await request(app)
+      .get("/chat/idlevideo")
+      .set("authorization", "jwtToken")
+      .send();
+
+    expect(response.status).to.equal(404);
+    expect(response.body.message).to.equal("Partner not found");
+    // Verify the function calls and stub invocations
+    sinon.assert.calledOnce(verifyStub);
+    sinon.assert.calledOnce(findPartnerStub);
+    sinon.assert.notCalled(findImageStub);
+    sinon.assert.notCalled(getIdleVideoURLStub);
+  });
+
+  it("should return an error if image is not found", async () => {
+    verifyStub.returns({ userId: "fakeUserId" });
+    findPartnerStub.resolves({ imageId: "fakeImageId" });
+    findImageStub.resolves(null);
+
+    const response = await request(app)
+      .get("/chat/idlevideo")
+      .set("authorization", "jwtToken")
+      .send();
+
+    expect(response.status).to.equal(404);
+    expect(response.body.message).to.equal("You haven't chosen partner yet");
+    // Verify the function calls and stub invocations
+    sinon.assert.calledOnce(verifyStub);
+    sinon.assert.calledOnce(findPartnerStub);
+    sinon.assert.calledOnce(findImageStub);
+    sinon.assert.notCalled(getIdleVideoURLStub);
+  });
 
 
-//   it("should return a video URL", async () => {
+  it("should get the idle video URL of the partner", async () => {
+    verifyStub.returns({ userId: "fakeUserId" });
+    findPartnerStub.resolves({ imageId: "fakeImageId" });
+    findImageStub.resolves({ videoURL: null, videoId: "fakeVideoId" });
+    getIdleVideoURLStub.resolves({ videoURL: "fakeVideoURL"})
+    // Stub the save method of the Image model
+    findImageStub.resolves({
+      save: sinon.stub().resolves(true)
+    });
 
-//     await request(app)
-//       .post("/partner/create")
-//       .send({"name": testPartner.name})
-//       .set("authorization", jwtTokenForTest);
+    const response = await request(app)
+      .get("/chat/idlevideo")
+      .set("authorization", "jwtToken")
+      .send();
 
-//     const partner = await Partner.findOne({ name: testPartner.name });
-//     const image = await Image.findOne({ imgBase64: partner.imageId });
-//     image.videoURL = "test-video-url";
-//     await image.save();
+    expect(response.status).to.equal(200);
+    expect(response.body.videoURL.videoURL).to.equal("fakeVideoURL");
+    // Verify the function calls and stub invocations
+    sinon.assert.calledOnce(verifyStub);
+    sinon.assert.calledOnce(findPartnerStub);
+    sinon.assert.calledOnce(findImageStub);
+    sinon.assert.calledOnce(getIdleVideoURLStub);
+  });
 
-//     const res = await request(app)
-//       .get("/chat/idleVideo")
-//       .set("authorization", jwtTokenForTest);
-//       console.log("res",res);
-//       expect(res.status).to.equal(200);
-//       expect(res.body).to.have.property("videoURL", "test-video-url");
+  it("should return an error if an exception is thrown", async () => {
+    verifyStub.returns({ userId: "fakeUserId" });
 
-//     await Partner.findOneAndDelete({ name: testPartner.name });
-//     await Image.findOneAndDelete({ imgBase64: partner.imageId });
+    // Simulate database error if an exception is thrown
+    findPartnerStub.throws({
+      name: "CustomError",
+      message: "Custom error message",
+    });
 
-//   });
+    const response = await request(app)
+      .get("/chat/idlevideo")
+      .set("authorization", "jwtToken")
+      .send();
 
-// });
+    // Perform assertions on the response
+    expect(response.status).to.equal(409);
+    expect(response.body.message).to.equal("CustomError Custom error message");
+    // Verify the function calls and stub invocations
+    sinon.assert.calledOnce(verifyStub);
+    sinon.assert.calledOnce(findPartnerStub);
+    sinon.assert.notCalled(findImageStub);
+    sinon.assert.notCalled(getIdleVideoURLStub);
+  });
+});
+
+describe("getChatHistory", () => {
+  let verifyStub;
+  let findChatStub;
+
+  beforeEach(() => {
+    // Stub the JWT verification function
+    verifyStub = sinon.stub(jwt, "verify");
+    findChatStub = sinon.stub(Chat, "findOne");
+  });
+
+  afterEach(() => {
+    verifyStub.restore();
+    findChatStub.restore();
+  });
+
+  it("should return an error if chat is not found", async () => {
+    verifyStub.returns({ userId: "fakeUserId" });
+    // Simulate chat not found
+    findChatStub.resolves(null);
+
+    const response = await request(app)
+      .get("/chat/chatHistory")
+      .set("authorization", "jwtToken");
+
+    expect(response.status).to.equal(404);
+    expect(response.body.message).to.equal("Partner not found");
+    // Verify the function calls and stub invocations
+    sinon.assert.calledOnce(verifyStub);
+    sinon.assert.calledOnce(findChatStub);
+  });
+
+  it("should get the chat history of the user", async () => {
+    verifyStub.returns({ userId: "fakeUserId" });
+
+    const chat = {
+      userId: "fakeUserId",
+      messages: [
+        { role: "user", content: "Hello" },
+        { role: "bot", content: "Hi there" },
+      ],
+    };
+
+    findChatStub.resolves(chat); // Simulate chat found
+
+    const response = await request(app)
+      .get("/chat/chatHistory")
+      .set("authorization", "jwtToken");
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({ chatHistory: chat.messages });
+    // Verify the function calls and stub invocations
+    sinon.assert.calledOnce(verifyStub);
+    sinon.assert.calledOnce(findChatStub);
+  });
+
+  it("should return an error if an exception is thrown", async () => {
+    verifyStub.returns({ userId: "fakeUserId" });
+    // Simulate database error if an exception is thrown
+    findChatStub.throws({
+      name: "CustomError",
+      message: "Custom error message",
+    });
+
+    const response = await request(app)
+      .get("/chat/chatHistory")
+      .set("authorization", "jwtToken");
+
+    expect(response.status).to.equal(409);
+    expect(response.body.message).to.equal("CustomError Custom error message");
+    // Verify the function calls and stub invocations
+    sinon.assert.calledOnce(verifyStub);
+    sinon.assert.calledOnce(findChatStub);
+  });
+});
